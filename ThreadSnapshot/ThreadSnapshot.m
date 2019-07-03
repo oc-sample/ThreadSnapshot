d732ee95ad909c93f56d4bea6c13eafb9a8439b5
郑俊明 <mjsecurity@126.com>
2019-07-04 00:11:37 +0800

    init

---
+++ ThreadSnapshot/ThreadSnapshot.m
@@ -0,0 +1,18 @@
+//
+//  ThreadSnapshot.m
+//  ThreadSnapshot
+//
+//  Created by 郑俊明 on 2019/7/3.
+//  Copyright © 2019 郑俊明. All rights reserved.
+//
+
+#import "ThreadSnapshot.h"
+
+@implementation ThreadSnapshot
+
+-(NSString*) generateSnapshot
+{
+    return @"";
+}
+
+@end
